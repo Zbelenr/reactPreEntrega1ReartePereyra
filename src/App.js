8162ffe67ba3496fef52305315d8da1d59f1a454
zbelenr <zrpereyra@gmail.com>
2023-02-01 11:55:11 -0300

    inicio de proyecto

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Hola como estan?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          documentacion
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
